9b3408227cbb9fd8b71ccc7ced0543cd22d9b5df
damarudhvarma <damarudhvarma.dandu@gmail.com>
2026-07-01 17:34:00 +0530

    item checkout

--- src/test/java/Capstone_project/Capstone_project/TestSignup.java
+++ src/test/java/Capstone_project/Capstone_project/TestSignup.java
@@ -8,16 +8,22 @@ public class TestSignup {
 	public static void main(String[] args) throws InterruptedException {
 	
 		// TODO Auto-generated method stub
-		AuthPOM ob = new AuthPOM(driver);
-		ob.signup();        
-		ob.signup_firstname("damarudh");
-        ob.signup_lastname("varma");
-        ob.signup_email("varma@example.com");
-        ob.signup_password("Varma@12342$");
-        Thread.sleep(2000);
+//		AuthPOM ob = new AuthPOM(driver);
+//		ob.signup();        
+//		ob.signup_firstname("damarudh");
+//        ob.signup_lastname("varma");
+//        ob.signup_email("varma@example.com");
+//        ob.signup_password("Varma@12342$");
+//        Thread.sleep(2000);
+//        
+//        ob.signup_checkbox();
+//        ob.signup_create();
         
-        ob.signup_checkbox();
-        ob.signup_create();
+        CheckoutPage cp = new CheckoutPage(driver);
+        cp.selectProduct();
+       // cp.clickCheckout();
+        cp.fillingShippingDetails("fsname","lasname");
+        cp.addAddress("23-/4anfhfj sdch","50097","hyderabad");
 		
 
 	}
